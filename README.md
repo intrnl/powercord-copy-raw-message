ce0e5161a4717893dbe3bf40441b32ef8f0c1f93
intrnl <intrnl28+git@gmail.com>
2019-10-27 14:27:11 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Powercord - Copy Raw Message
+
+Lets you copy messages with its formatting still intact, you should enable
+Developer Mode in Appearance settings
---
+++ index.js
@@ -0,0 +1,47 @@
+const { Plugin } = require('powercord/entities')
+const { ContextMenu: { Button: CMButton } } = require('powercord/components')
+const { React, getModuleByDisplayName } = require('powercord/webpack')
+const { inject, uninject } = require('powercord/injector')
+
+
+class CopyRawMessage extends Plugin {
+  startPlugin () {
+    this._patchContextMenu()
+  }
+
+  pluginWillUnload () {
+    uninject('powercord-copy-raw-message_cm')
+  }
+
+  _patchContextMenu () {
+    const FluxMessageDevModeCMG = getModuleByDisplayName('FluxContainer(MessageDeveloperModeGroup)', false)
+    const MessageDevModeCMG = (new FluxMessageDevModeCMG).render().type
+
+    inject('powercord-copy-raw-message_cm', MessageDevModeCMG.prototype, 'render', function (_, res) {
+      res.props.children.push(
+        React.createElement(CMButton, {
+          name: 'Copy Raw Message',
+          onClick: () => {
+            const el = document.createElement('textarea')
+            el.style.opacity = 0;
+            el.value = this.props.message.content
+            
+            document.body.append(el)
+
+            el.select()
+            document.execCommand('copy')
+
+            el.remove()
+
+            // Have to find the proper method, for now this will do.
+            document.body.click()
+          },
+        }),
+      )
+
+      return res
+    })
+  }
+}
+
+module.exports = CopyRawMessage
