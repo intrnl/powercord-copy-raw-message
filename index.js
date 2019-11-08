5e617fd602c5dc6d2d95db702f1eb345340e9096
intrnl <intrnl28+git@gmail.com>
2019-11-09 00:59:53 +0700

    Linting

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-const { Plugin } = require('powercord/entities')
-const { ContextMenu: { Button: CMButton } } = require('powercord/components')
-const { React, getModuleByDisplayName } = require('powercord/webpack')
-const { inject, uninject } = require('powercord/injector')
+const { Plugin } = require('powercord/entities');
+const { ContextMenu: { Button: CMButton } } = require('powercord/components');
+const { React, getModuleByDisplayName } = require('powercord/webpack');
+const { inject, uninject } = require('powercord/injector');
 
 
 class CopyRawMessage extends Plugin {
   startPlugin () {
-    this._patchContextMenu()
+    this._patchContextMenu();
   }
 
   pluginWillUnload () {
-    uninject('powercord-copy-raw-message_cm')
+    uninject('powercord-copy-raw-message_cm');
   }
 
   async _patchContextMenu () {
-    const FluxMessageDevModeCMG = await getModuleByDisplayName('FluxContainer(MessageDeveloperModeGroup)')
-    const MessageDevModeCMG = (new FluxMessageDevModeCMG).render().type
+    const FluxMessageDevModeCMG = await getModuleByDisplayName('FluxContainer(MessageDeveloperModeGroup)');
+    const MessageDevModeCMG = (new FluxMessageDevModeCMG).render().type;
 
     inject('powercord-copy-raw-message_cm', MessageDevModeCMG.prototype, 'render', function (_, res) {
       res.props.children.push(
         React.createElement(CMButton, {
           name: 'Copy Raw Message',
           onClick: () => {
-            const el = document.createElement('textarea')
+            const el = document.createElement('textarea');
             el.style.opacity = 0;
-            el.value = this.props.message.content
-            
-            document.body.append(el)
+            el.value = this.props.message.content;
 
-            el.select()
-            document.execCommand('copy')
+            document.body.append(el);
 
-            el.remove()
+            el.select();
+            document.execCommand('copy');
+
+            el.remove();
 
             // Have to find the proper method, for now this will do.
-            document.body.click()
+            document.body.click();
           },
         }),
-      )
+      );
 
-      return res
-    })
+      return res;
+    });
   }
 }
 
-module.exports = CopyRawMessage
+module.exports = CopyRawMessage;
